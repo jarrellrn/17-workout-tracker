8727378f2ab8d5099007a941bede1ac43782af21
jarrellrn <63278388+jarrellrn@users.noreply.github.com>
2020-08-18 11:09:21 -0500

    trying to get api working

--- models/workout.js
+++ models/workout.js
@@ -3,40 +3,42 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
   exercises: [
     {
       type: {
         type: String,
         trim: true,
-        required: "Enter exercise type",
       },
       name: {
         type: String,
         trim: true,
-        required: "Enter exercise name",
       },
-      duration: {
-        type: Number,
-        required: "Enter workout time (minutes)",
-      },
+      duration: Number,
       weight: {
         type: Number,
+        default: 0,
       },
       reps: {
         type: Number,
+        default: 0,
       },
       sets: {
         type: Number,
+        default: 0,
       },
       distance: {
         type: Number,
+        default: 0,
       },
     },
   ],
-
-  day: {
-    type: Date,
-    default: () => new Date(),
+  totalDuration: {
+    type: Number,
+    default: 0,
   },
 });
 
